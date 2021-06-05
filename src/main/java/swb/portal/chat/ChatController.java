79b08de01b5900f2882ea7cd4750e1eeebfee9df
Sebastian <sebastian.korytkowski@student.wat.edu.pl>
2021-06-05 17:15:25 +0200

    Moved code to ChatService and made it thread safe.

--- src/main/java/swb/portal/chat/ChatController.java
+++ src/main/java/swb/portal/chat/ChatController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import swb.portal.chat.dto.ChatEventDto;
 import swb.portal.chat.dto.ChatMessageDto;
+import swb.portal.role.RoleService;
 
 import java.security.Principal;
 import java.util.*;
@@ -22,101 +23,29 @@ import java.util.*;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ChatController {
-    private static final String WS_MESSAGE_TRANSFER_DESTINATION = "/topic/chat";
-    private final SimpMessagingTemplate simpMessagingTemplate;
-
-
-    private Queue<ChatPrincipal> searchQueue = new ArrayDeque<>();
-    private Set<ChatSession> chatSessions = new HashSet<>();
-
-    private void createChatSession(ChatPrincipal firstUser, ChatPrincipal secondUser)
-    {
-        ChatSession chatSession = new ChatSession(firstUser, secondUser);
-
-
-        chatSessions.add(chatSession);
-        firstUser.setCurrentSession(chatSession);
-        secondUser.setCurrentSession(chatSession);
-
-        simpMessagingTemplate.convertAndSendToUser(
-                firstUser.getName(),
-                WS_MESSAGE_TRANSFER_DESTINATION,
-                new ChatEventDto(ChatEventDto.EventType.FOUND));
-
-        simpMessagingTemplate.convertAndSendToUser(
-                secondUser.getName(),
-                WS_MESSAGE_TRANSFER_DESTINATION,
-                new ChatEventDto(ChatEventDto.EventType.FOUND));
-
-    }
-
-    private void removeChatSession(ChatSession chatSession)
-    {
-        ChatPrincipal firstUser = chatSession.getFirstUser();
-        ChatPrincipal secondUser = chatSession.getSecondUser();
-
-        chatSessions.remove(chatSession);
-        firstUser.setCurrentSession(null);
-        secondUser.setCurrentSession(null);
-
-        simpMessagingTemplate.convertAndSendToUser(
-                firstUser.getName(),
-                WS_MESSAGE_TRANSFER_DESTINATION,
-                new ChatEventDto(ChatEventDto.EventType.DISCONNECTED));
-
-        simpMessagingTemplate.convertAndSendToUser(
-                secondUser.getName(),
-                WS_MESSAGE_TRANSFER_DESTINATION,
-                new ChatEventDto(ChatEventDto.EventType.DISCONNECTED));
-    }
+    private final ChatService chatService;
 
     @MessageMapping("/chat.search")
     public void searchForPartner(SimpMessageHeaderAccessor sha) {
         ChatPrincipal user = (ChatPrincipal) sha.getUser();
-
-        if(!searchQueue.contains(user)) {
-            searchQueue.add(user);
-
-            if (searchQueue.size() >= 2) {
-                createChatSession(searchQueue.poll(), searchQueue.poll());
-            }
-        }
+        chatService.startSearchForPartner(user);
     }
 
     @EventListener
     public void onDisconnectEvent(SessionDisconnectEvent event) {
         ChatPrincipal user = (ChatPrincipal) event.getUser();
-
-        searchQueue.remove(user);
-
-        ChatSession currentChatSession = user.getCurrentSession();
-        if(currentChatSession!=null) {
-            removeChatSession(currentChatSession);
-        }
+        chatService.disconnectUser(user);
     }
 
     @MessageMapping("/chat.send")
     public void sendMessage(@Payload ChatMessageDto chatMessage, SimpMessageHeaderAccessor sha) {
         ChatPrincipal sender = (ChatPrincipal) sha.getUser();
-        ChatSession chatSession = sender.getCurrentSession();
-
-        if(chatSession != null) {
-            ChatPrincipal target = chatSession.getOther(sender);
-
-            simpMessagingTemplate.convertAndSendToUser(
-                    target.getName(),
-                    WS_MESSAGE_TRANSFER_DESTINATION,
-                    chatMessage);
-        }
+        chatService.sendMessage(sender, chatMessage);
     }
 
     @MessageMapping("/chat.leave")
     public void leaveChat(SimpMessageHeaderAccessor sha) {
-        ChatPrincipal sender = (ChatPrincipal) sha.getUser();
-        ChatSession chatSession = sender.getCurrentSession();
-
-        if(chatSession != null) {
-            removeChatSession(chatSession);
-        }
+        ChatPrincipal user = (ChatPrincipal) sha.getUser();
+        chatService.leaveSession(user);
     }
 }
